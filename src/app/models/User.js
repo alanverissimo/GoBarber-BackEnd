4a49e012c4a8cca9c5f7639a4df764cc13f9baca
Alan Verissimo <alan_verissimo@hotmail.com>
2022-05-21 02:48:57 -0300

    criação token

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from "sequelize";
+import bcrypt from "bcryptjs";
 
 class User extends Model {
   static init(sequelize) {
@@ -15,6 +16,17 @@ class User extends Model {
       }
     );
 
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+
+    }
+    checkPassword(password) {
+      return bcrypt.compare(password, this.password_hash);
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 import User from "./app/models/User";
 
 import UserController from "./app/controllers/UserController";
+import SessionController from "./app/controllers/SessionController";
 
 const routes = new Router();
 
 routes.post("/users", UserController.store);
+routes.post("/sessions", SessionController.store);
 
 export default routes;
