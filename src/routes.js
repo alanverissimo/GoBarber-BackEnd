dd33b7b1a0b05236363bc405cf59a4e8ac24f670
Alan Verissimo <alan_verissimo@hotmail.com>
2022-05-21 02:09:38 -0300

    cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,10 @@
 import { Router } from "express";
 import User from "./app/models/User";
 
+import UserController from "./app/controllers/UserController";
+
 const routes = new Router();
 
-routes.get("/", async (req, res) => {
-    const user = await User.create({
-        name: "Alan Verissimo",
-        email: "alan_verissimo@hotmail.com",
-        password_hash: "12345678",
-    });
-
-return res.json(user);
-});
+routes.post("/users", UserController.store);
 
 export default routes;
