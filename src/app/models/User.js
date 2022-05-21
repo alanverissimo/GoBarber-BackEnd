c85de38f828c7302f134b67f582d7444046fa596
Alan Verissimo <alan_verissimo@hotmail.com>
2022-05-21 19:48:48 -0300

    Avatar do usuario

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,5 @@
-import Sequelize, { Model } from "sequelize";
-import bcrypt from "bcryptjs";
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -15,7 +15,6 @@ class User extends Model {
         sequelize,
       }
     );
-
     this.addHook('beforeSave', async user => {
       if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 8);
@@ -23,11 +22,15 @@ class User extends Model {
     });
 
     return this;
+  }
 
-    }
-    checkPassword(password) {
-      return bcrypt.compare(password, this.password_hash);
-    }
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
 }
 
 export default User;
