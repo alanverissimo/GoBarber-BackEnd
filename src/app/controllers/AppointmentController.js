f3c403cc43617dcdb114f9b17071489c538f3c89
Alan Verissimo <alan_verissimo@hotmail.com>
2022-05-22 03:18:58 -0300

    second commit

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import Appointment from '../models/Appointment';
-import { startOfHour, parseISO, isBefore } from "date-fns";
+import { startOfHour, parseISO, isBefore, format } from "date-fns";
+import pt from "date-fns/locale/pt";
 import User from '../models/User';
 import * as Yup from 'yup';
 import File from '../models/File';
+import Notification from "../schemas/Notification";
 
 class AppointmentController {
 
@@ -81,6 +83,17 @@ class AppointmentController {
             provider_id,
             date: hourStart,
 
+        });
+
+        // Notify appointment provider
+        
+        
+        const user = await User.findByPk(req.userId);
+        const formattedDate = format(hourStart, "'dia' dd 'de' MMMM', às' H:mm'h'", { locale: pt});
+
+        await Notification.create({
+            content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+            user: provider_id,
         });
     
         return res.json(appointment);       
