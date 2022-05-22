d5f7d8ff4c15ce9066d66ce0d7756f6dc40a9447
Alan Verissimo <alan_verissimo@hotmail.com>
2022-05-22 16:44:04 -0300

    second commit

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-    host: "smtp.mailtrap.io",
-    port: 2525,
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
     secure: false,
     auth: {
-      user: "f00119a4796eb4",
-      pass: "0a6c3478cea2f5",
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
     },
     default: {
       from: "Equipe GoBarber <noreply@gobarber.com>",
---
+++ src/config/sentry.js
@@ -0,0 +1,8 @@
+export default {
+    dsn: process.env.SENTRY_DSN
+    
+    // "https://c204fa4607664faf80fade8136d55f3a@o1257950.ingest.sentry.io/6431577",
+  };
+  
+  
+ 
